3d83aea1e62dcc0dadd08461eff6196b8f7e9931
nashby <younash@gmail.com>
2010-05-29 22:08:50 +0300

    regexp fix

--- upload.cmd
+++ upload.cmd
@@ -3,7 +3,7 @@ cls
 
 curl -# -F "qbfile=@%1" -F "valid=1" -F "form_submit=yes" http://file.qb.by/upload > %TEMP%\qb.txt"
 
-for /F "delims=" %%a in ('findstr /R "http://file.qb.by/[a-z0-9]" %TEMP%\qb.txt') do set var="%%a"
+for /F "delims=" %%a in ('findstr /R "http://file.qb.by/*[a-z0-9]" %TEMP%\qb.txt') do set var="%%a"
 
 echo.%var:~39,50% | clip
 echo "Done! Press Ctrl+V and get your link"
