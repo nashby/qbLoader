83963e92a0ad83168db53d4e89327c34ee1f0601
nashby <younash@gmail.com>
2010-06-06 15:35:09 +0300

    login add, can upload 5mb files

--- upload.cmd
+++ upload.cmd
@@ -1,7 +1,7 @@
 cls
 @echo off
-
-curl -# -F "qbfile=@%1" -F "valid=1" -F "form_submit=yes" http://file.qb.by/upload > %TEMP%\qb.txt"
+curl -F "password=qbLoader" -F "username=qbLoader" http://qb.by/login/ -c %TEMP%\qb_cookie.txt
+curl -b %TEMP%\qb_cookie.txt  -# -F "qbfile=@%1" -F "valid=1" -F "form_submit=yes" http://file.qb.by/upload > %TEMP%\qb.txt" 
 
 for /F "delims=" %%a in ('findstr /R "http://file.qb.by/*[a-z0-9]" %TEMP%\qb.txt') do set var="%%a"
 
