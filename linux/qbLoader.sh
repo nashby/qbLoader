de9b3c497564445d0480ea52fc9db1563d677aa3
nashby <younash@gmail.com>
2011-02-21 11:56:10 +0200

    xclip replaced with xsel

--- linux/qbLoader.sh
+++ linux/qbLoader.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-curl -F "password=qbLoader" -F "username=qbLoader" http://qb.by/login/ -c /tmp/qb_cookie.txt
-curl -b tmp/qb_cookie.txt -# -F "qbfile=@$NAUTILUS_SCRIPT_SELECTED_FILE_PATHS" -F "valid=1" -F "form_submit=yes" http://file.qb.by/upload | grep -E -o "http://file.qb.by/[a-z0-9]+" | xclip -selection clipboard
+file="`echo $NAUTILUS_SCRIPT_SELECTED_FILE_PATHS|tr -d "\n"`"
 
+touch ~/qb_cookie.txt
+curl -F "password=qbLoader" -F "username=qbLoader" http://qb.by/login/ -c ~/qb_cookie.txt
+link=`curl -b ~/qb_cookie.txt -# -F "qbfile=@$file" -F "valid=1" -F "form_submit=yes" http://file.qb.by/upload | grep -E -o "http://file.qb.by/[a-z0-9]+"` 
+echo $link | xsel --clipboard
+rm ~/qb_cookie.txt
 echo "Done! Press Ctrl+V and get your link"
 read
-
